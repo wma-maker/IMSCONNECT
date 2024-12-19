228cc0631ffca979d1a9965bfe2e5067db80723b
wma-maker <mahaw1981@gmail.com>
2024-12-19 19:55:00 +0500

    Update auth.js

--- backend/api/auth.js
+++ backend/api/auth.js
@@ -1,16 +1,22 @@
 // api/auth/login.js
-const authMiddleware = require('../middleware/authmiddleware');
+//const authMiddleware = require('../middleware/authmiddleware');
 const { login } = require('../controllers/authcontroller');
+const express = require('express');
+const router = express.Route();
 
-module.exports = (req, res) => {
+//module.exports = (req, res) => {
   // Apply the authentication middleware first
-  authMiddleware(req, res, () => {
-    if (req.method === 'POST') {
-      return login(req, res);
-    }
+//  authMiddleware(req, res, () => {
+   // if (req.method === 'POST') {
+   //   return login(req, res);
+   // }
 
     // Handle unsupported methods
-    res.status(405).json({ message: 'Method Not Allowed' });
-  });
-};
+   // res.status(405).json({ message: 'Method Not Allowed' });
+  //});
+//};
+router.post('/login', login);
+
+module.exports = router;
+//adjusted new
 
